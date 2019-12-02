efcbe74094f031f41a1edbf4334578477fe7397c
Administrator <97655256@qq.com>
2019-12-02 15:09:19 +0800

    springboot框架

--- src/main/java/first/controller/UserController.java
+++ src/main/java/first/controller/UserController.java
@@ -1,7 +1,12 @@
 package first.controller;
 
+import first.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserService userService;
 }
